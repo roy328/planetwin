0571fe3bc8e2ca95304e979d3370cf53d9299fbd
roy328 <royandrew328@outlook.com>
2024-11-17 08:06:15 -0800

    Inital commit

---
+++ scripts/game.py
@@ -0,0 +1,11 @@
+class Game():
+    def __init__(self) -> None:
+        self.delay = 1
+        self.hand = []
+
+    def run(self) -> None:
+        print("running....")
+            
+if __name__ == "__main__":
+    play = Game()
+    play.run
